83adca30b675baed6545338830d186b072fa8b44
RmanKartavcev <128297080+Ramzes8@users.noreply.github.com>
2023-04-09 21:40:40 +0900

    added

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,19 +1,22 @@
-import pytest, json
-
-from utils import get_data, get_filtered_data, get_last_values, get_formated_data
+import pytest
+from utils import get_data
+from utils import get_filtered_data, get_last_values, get_formated_data
 
 
 def test_get_data():
     data = get_data()
     assert isinstance(data, list)
 
+
 def test_filtered_data(test_data):
     assert len(get_filtered_data(test_data, filtered_empty_from=False)) == 3
     assert len(get_filtered_data(test_data, filtered_empty_from=True)) == 3
 
+
 def test_get_last_values(test_data):
     data = get_last_values(test_data, 2)
     assert [x['date'] for x in data] == ["2019-08-26T10:50:58.294041", "2019-07-03T18:35:29.512364"]
+
 
 def test_get_formated_data(test_data):
     data = get_formated_data(test_data[:1])
