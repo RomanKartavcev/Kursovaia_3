4094676680a4f86d9d8165aa1013b06d5dd0d64b
RmanKartavcev <128297080+Ramzes8@users.noreply.github.com>
2023-04-09 22:49:35 +0900

    added

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -4,21 +4,25 @@ from utils import get_filtered_data, get_last_values, get_formated_data
 
 
 def test_get_data():
+    """ Тестирование получения данных"""
     data = get_data()
     assert isinstance(data, list)
 
 
 def test_filtered_data(test_data):
+    """ Тестирование фильтрации данных"""
     assert len(get_filtered_data(test_data, filtered_empty_from=False)) == 3
     assert len(get_filtered_data(test_data, filtered_empty_from=True)) == 3
 
 
 def test_get_last_values(test_data):
+    """ Тестирование получения данных по дате"""
     data = get_last_values(test_data, 2)
     assert [x['date'] for x in data] == ["2019-08-26T10:50:58.294041", "2019-07-03T18:35:29.512364"]
 
 
 def test_get_formated_data(test_data):
+    """ Тестирование форматирования данных"""
     data = get_formated_data(test_data[:1])
     assert data[0] == "\n26.08.2019 Перевод организации \nMaestro 1596 83** **** 5199 -> Счет **9589\n31957.58 руб."
     data = get_formated_data(test_data[1:2])
